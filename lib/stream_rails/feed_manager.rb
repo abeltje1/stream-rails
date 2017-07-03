685bbebbe9b1c5ea8892397f75b9d18d4f357407
abeltje1 <abelvanhoek@gmail.com>
2017-07-03 16:01:01 +0200

    also make it possible to follow a non-user feed

--- lib/stream_rails/feed_manager.rb
+++ lib/stream_rails/feed_manager.rb
@@ -34,6 +34,15 @@ module StreamRails
         news_feed.follow(target_feed.slug, target_feed.user_id)
       end
     end
+    
+    def follow_feed(feed_type, user_id, target_id)
+      return unless StreamRails.enabled?
+      target_feed = get_feed(feed_type, target_id)
+      @news_feeds.each do |_, feed|
+        news_feed = get_feed(feed, user_id)
+        news_feed.follow(target_feed.slug, target_feed.user_id)
+      end 
+    end
 
     def unfollow_user(user_id, target_id)
       return unless StreamRails.enabled?
